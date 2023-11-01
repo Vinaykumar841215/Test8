04608fc276a791835003014ab9e7236b4fdc966b
A R <vinaykumar071202@gmail.com>
2023-11-01 09:57:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/test8/DataClass.kt
@@ -0,0 +1,13 @@
+package com.example.test8
+
+data class DataClass(
+    val name:String?=null,
+    val age :Long?=null,
+    val email: String?=null,
+    val address:String?=null,
+    val userId:String?=null
+
+
+) {
+
+}
---
+++ app/src/main/java/com/example/test8/DataModel.kt
@@ -0,0 +1,9 @@
+package com.example.test8
+
+data class DataModel(
+    val name:String?=null,
+    val desciption:String?=null,
+    val price:Long?=null,
+    val image:String?=null,
+    val userId:String?=null
+)
